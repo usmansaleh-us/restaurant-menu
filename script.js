e765a39ba63295903223de1a0291dda2d5817e46
usmansaleh-us <usmanauwalusaleh@gmail.com>
2025-08-22 01:48:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Collapse/Expand Categories
-document.querySelectorAll(".menu-category h2").forEach(categoryHeader => {
-  categoryHeader.addEventListener("click", () => {
-    const category = categoryHeader.parentElement;
-    category.classList.toggle("collapsed");
-  });
-});
 
 function showCategory(categoryId) {
     let categories = document.querySelectorAll('.menu-category');
@@ -12,8 +5,3 @@ function showCategory(categoryId) {
         cat.classList.add('hidden');
     });
     document.getElementById(categoryId).classList.remove('hidden');
-// Toggle category visibility
-function toggleCategory(element) {
-  const items = element.nextElementSibling;
-  items.style.display = (items.style.display === "block") ? "none" : "block";
-}
